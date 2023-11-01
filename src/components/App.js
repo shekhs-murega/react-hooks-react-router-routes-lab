d0256e2b3331fe124520c0adbd9eab97dcae5f75
shekinah <nyamburamary1212@gmail.com>
2023-11-01 12:42:14 +0300

    Assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
 import Movies from "./Movies";
-
+import Directors from "./Directors";
+import Actors from "./Actors";
+import './App.css'
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+    <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/actors" component={Actors} />
+        <Route path="/directors" component={Directors} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <div className="navbar">
+        <NavLink to="/" activeClassName="active" exact>
+          Home
+        </NavLink>
+        <NavLink to="/movies" activeClassName="active">
+          Movies
+        </NavLink>
+        <NavLink to="/actors" activeClassName="active">
+          Actors
+        </NavLink>
+        <NavLink to="/directors" activeClassName="active">
+          Directors
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
